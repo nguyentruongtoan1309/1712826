ad93e7b979a77a51eb4cbf58b2f5dcdac2516c83
nguyentruongtoan1309 <37992868+nguyentruongtoan1309@users.noreply.github.com>
2018-04-24 21:00:52 +0700

    tai cap phat bo nho va tach thong tin

--- 1712826/1712826.cpp
+++ 1712826/1712826.cpp
@@ -49,3 +49,66 @@ void ChuanHoaThongTin(string &SinhVien)
 		}
 	}
 }
+//Hàm tái cấp phát bộ nhớ
+void TaiCapPhatBoNho(string *&SinhVien_CT, int SV_index)
+{
+	string *temp = new string[SV_index];
+	for (int i = 0; i < SV_index; i++)
+	{
+		*(temp + i) = *(SinhVien_CT + i);
+	}
+	SinhVien_CT = new string[SV_index + 1];
+	for (int i = 0; i < SV_index; i++)
+	{
+		*(SinhVien_CT + i) = *(temp + i);
+	}
+	delete[] temp;
+}
+
+//Hàm tách thông tin từ chuỗi thông tin các sinh viên
+void TachThongTin(string SinhVien, ThongTin SV[], int index, string *&SinhVien_CT, int SV_index, int *&ThuocTinh)
+{
+	//Chuẩn hóa chuỗi thông tin
+	ChuanHoaThongTin(SinhVien);
+
+	//Chuyển string thành char *
+	char *c = (char*)SinhVien.c_str();
+	char *nl = NULL;
+	char temp[] = "\"";
+
+	//Cấp phát vùng nhớ cho SinhVien_CT và gán số thuộc tính của sinh viên là 0
+	SinhVien_CT = new string[1];
+	ThuocTinh[index] = 0;
+
+	//Tách dữ liệu từ file
+	char *p = strtok_s(c, temp, &nl);
+	while (p != NULL)
+	{
+		int n = strlen(p);
+		for (int i = 0; i < n; i++)
+		{
+			SinhVien_CT[SV_index].push_back(p[i]);
+		}
+		SV_index++;
+		TaiCapPhatBoNho(SinhVien_CT, SV_index);
+		p = strtok_s(NULL, temp, &nl);
+		//Cộng thuộc tính với số gia 1
+		ThuocTinh[index] += 1;
+	}
+
+	//Đưa dữ liệu vào trong struct (trừ sở thích)
+	SV[index].MSSV = SinhVien_CT[0];
+	SV[index].Ten = SinhVien_CT[1];
+	SV[index].Khoa = SinhVien_CT[2];
+	SV[index].Email = SinhVien_CT[3];
+	SV[index].NgaySinh = SinhVien_CT[4];
+	SV[index].HinhAnh = SinhVien_CT[5];
+	SV[index].MoTa = SinhVien_CT[6];
+
+	//Đưa sở thích vào struct
+	SV[index].SoThich = new string[SV_index - 6];
+	for (int i = 0; i < SV_index - 6; i++)
+	{
+		SV[index].SoThich[i] = SinhVien_CT[i + 7];
+	}
+}
