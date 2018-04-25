3bc518c7fc7ab78b18ab9fc609ac35f272cbaa0f
nguyentruongtoan1309 <37992868+nguyentruongtoan1309@users.noreply.github.com>
2018-04-25 14:53:39 +0700

    Ham get template

--- 1712826/1712826.cpp
+++ 1712826/1712826.cpp
@@ -252,3 +252,26 @@ void TaiThongTin(ThongTin *&SV, string HTML, int index, int *ThuocTinh)
 	f1 << HTML;
 	f1.close();
 }
+//Hàm Get Template
+//Input: chuỗi HTML
+//Output: dữ liệu html template chứa vào chuỗi HTML
+void GetTemplate(string &HTML)
+{
+	//Mở file html
+	ifstream f;
+	f.open("Personal.txt");
+	if (!f)
+	{
+		perror("Mo file that bai.\n");
+	}
+	string T; //Chuỗi tạm
+	while (!f.eof())
+	{
+		getline(f, T);
+		HTML += T;
+		HTML += "\n";
+	}
+
+	//Đóng file
+	f.close();
+}
